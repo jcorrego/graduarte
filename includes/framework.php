fc068a3ffd76fd182bf2fddb4b541184feeb87ff
Juan Carlos Orrego <jcorrego@gmail.com>
2016-05-04 08:29:02 -0500

    PHP 7 compatibility changes reverted

--- includes/framework.php
+++ includes/framework.php
@@ -18,12 +18,8 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
  * Joomla! system checks
  */
 
-if(get_magic_quotes_runtime())
-{
-	// Deactivate
-	set_magic_quotes_runtime(false);
-}
 
+@set_magic_quotes_runtime( 0 );
 @ini_set('zend.ze1_compatibility_mode', '0');
 
 /*
